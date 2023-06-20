9bf2d35654ba3d4c753597e8f0b89dbe14ed89ca
skylarbsandler <skylarbsandler@gmail.com>
2023-06-20 09:46:52 -0600

    Added Wagon Class

--- Caravan/Passenger.cs
+++ Caravan/Passenger.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Caravan
 {
-    internal class Passenger
+    public class Passenger
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Destination { get; set; }
+        public Wagon Wagon { get; set; }
     }
 }
---
+++ Caravan/Wagon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan
+{
+    public class Wagon
+    {
+        public string Name { get; set; }
+        public int NumWheels { get; set; }
+        public bool Covered { get; set; }
+        public List<Passenger> Passengers { get; set; }
+
+        public Wagon (string name, int numwheels, bool covered)
+        {
+            Name = name;
+            NumWheels = numwheels;
+            Covered = covered;
+
+        }
+    }
+}
